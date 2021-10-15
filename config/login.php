9f9d1aed064ee655e24fab9c08de1f20b79a9d74
Smykov Sergei <s.smikow@outlook.com>
2021-10-15 18:18:18 +0300

    Начало создания

---
+++ config/login.php
@@ -0,0 +1,1 @@
+<section>Авторизация</section>
--- index.php
+++ index.php
@@ -1,12 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+$url = explode("?", $_SERVER["REQUEST_URI"]);
+$url = urldecode($url[0]);
+$url = explode("/", $url);
+$url = array_pop($url);
+
+$pagelink = $url == "" ? "index" : $url;
+
+if(!file_exists("config/$pagelink.php"))
+    $pagelink = "404";
+
+
+switch($pagelink)
+{
+    case "index":
+        $title = "Главаня";
+        break;
+    case "o_nas":
+        $title = "О нас";
+        break;
+    case "dostavka_oplata":
+        $title = "Доставка и оплата";
+        break;
+    case "feedback":
+        $title = "Отзывы";
+        break;
+    case "news":
+        $title = "Новости";
+        break;
+    case "contacts":
+        $title = "Контакты";
+        break;
+    case "login":
+        $title = "Авторизация";
+        break;
+    case "reg":
+        $title = "Регистрация";
+        break;
+    default:
+        $title = "Ошибка";    
+}
+
+require "content/header.php";
+require "config/$pagelink.php";
+require "content/footer.php";
