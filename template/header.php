9ca6689cc0a0544c8f7b2fac8e3f2995dca1b74f
mudrec <s.smikow@outlook.com>
2021-10-24 17:13:07 +0300

    mudrec

--- template/header.php
+++ template/header.php
@@ -1,23 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/reset.php">
-    <link rel="stylesheet" href="css/style.php">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title><?= $title?></title>
 </head>
 <body>
 <header>
     <div class="contener">
-        <ul>
-            <li><a href="index">Главаная</a></li>
-            <li><a href="o_nas">О нас</a></li>
-            <li><a href="dostavka_i_oplata">Доставка и оплата</a></li>
-            <li><a href="feedback">Отзывы</a></li>
-            <li><a href="news">Новости</a></li>
-            <li><a href="contacts">Контакты</a></li>
-        </ul>
+        <div class="header-top">
+            <ul>
+                <li><a href="index">Главаная</a></li>
+                <li><a href="o_nas">О нас</a></li>
+                <li><a href="dostavka_i_oplata">Доставка и оплата</a></li>
+                <li><a href="feedback">Отзывы</a></li>
+                <li><a href="news">Новости</a></li>
+                <li><a href="contacts">Контакты</a></li>
+            </ul>
+            <div class="cabinet">
+                <a href="">Личный кабинет</a>
+            </div>
+            <div class="cart">
+                <a href="cart"><i>Корзина</i></a>
+                <p>450 <span>руб</span>.</p>
+            </div>
+        </div>
+        <div class="header-mid">
+            <div class="logo">
+                <a href="index">
+                    <i>logo</i>
+                    <span>Экспресс</span>
+                    <p>Закусочная</p>
+                    <i>logo</i>
+                </a>
+            </div>
+        </div>
+        <div class="header-button"></div>
     </div>
 </header>
