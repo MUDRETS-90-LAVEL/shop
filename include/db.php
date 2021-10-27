aa91a20ed8783160a13f85102c99514ae4fb40da
Sergei <76744476+MUDRETS-90-LAVEL@users.noreply.github.com>
2021-10-27 11:32:13 +0400

    Update db.php

--- include/db.php
+++ include/db.php
@@ -5,7 +5,7 @@ define('DB_PASSWORD',''); //password db
 define('DB_NAME','shop'); //name db
 
 $mysql = @new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-    if ($mysql -> connect_errno) exit('Ошибка подключение к БД');
-$mysql -> set_charset('utf8');
-$mysql -> close();
+    if ($mysql->connect_errno) exit('Ошибка подключение к БД');
+$mysql->set_charset('utf8');
+// $mysql -> close();
 ?>
