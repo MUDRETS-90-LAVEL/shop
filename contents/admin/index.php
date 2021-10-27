3ca8065e26c382e1416b5856d99bb6c8d576b281
Sergei <76744476+MUDRETS-90-LAVEL@users.noreply.github.com>
2021-10-27 12:02:41 +0400

    Update index.php

--- contents/admin/index.php
+++ contents/admin/index.php
@@ -1,1 +1,16 @@
-<?php
+<section class="content">
+        <div class="box">
+            <img src="img/user.png" alt="user">
+            <form action="">
+                <div class="input-box">
+                    <i class="fa fa-user" aria-hidden="true"></i>
+                    <input type="text" name="login" placeholder="Введите ваш логин:">
+                </div>
+                <div class="input-box">
+                    <i class="fa fa-lock" aria-hidden="true"></i>
+                    <input type="password" name="password" placeholder="Введите ваш пароль:">
+                </div>
+                <input type="submit" value="Войти">
+            </form>
+        </div>
+    </section>
